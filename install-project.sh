d25b09a9c2ff63a36d73352a89efe79f655717db
winglam <winglam2@illinois.com>
2020-08-30 16:35:51 -0500

    Handling cases where SRA did not work for some modules.

--- install-project.sh
+++ install-project.sh
@@ -14,6 +14,25 @@ echo "================Installing the project"
 if [[ "$slug" == "apache/incubator-dubbo" ]]; then
     sudo chown -R $USER .
     mvn clean install -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
+elif [[ "$modifiedslug_with_sha" == "doanduyhai.achilles-e3099bd" ]]; then
+    sed -i "s?http://repo?https://repo?" pom.xml
+    mvn clean install -am -pl $module -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
+elif [[ "$modifiedslug_with_sha" == "logzio.sawmill-e493c2e" ]]; then
+    sed -i '20,48d' sawmill-core/pom.xml
+    mvn clean install -am -pl $module -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
+elif [[ "$modifiedslug_with_sha" == "apache.hadoop-cc2babc" ]]; then
+    sudo apt-get install autoconf automake libtool curl make g++ unzip;
+    wget -nv https://github.com/protocolbuffers/protobuf/releases/download/v2.5.0/protobuf-2.5.0.tar.gz;
+    tar -zxvf protobuf-2.5.0.tar.gz;
+    cd protobuf-2.5.0
+    ./configure; make -j15;
+    sudo make install;
+    sudo ldconfig;
+    cd ..
+    mvn clean install -am -pl $module -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
+elif [[ "$modifiedslug_with_sha" == "nationalsecurityagency.timely-3a8cbd3" ]]; then
+    sed -i '466s/\${sureFireArgLine}//' pom.xml
+    mvn clean install -am -pl $module -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
 else
     mvn clean install -am -pl $module -DskipTests ${MVNOPTIONS} |& tee mvn-install.log
 fi
