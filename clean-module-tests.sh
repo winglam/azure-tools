b213e94ecfe4889f72e86dc7f0388942cf2baf67
winglam <winglam2@illinois.com>
2020-09-01 00:28:34 -0500

    Add script to clean test list.

---
+++ clean-module-tests.sh
@@ -0,0 +1,15 @@
+# Meant to run inside azure-tools/module-summarylistgen
+
+# remove PUTs
+for f in $(grep "\\["  *.csv ); do # ]
+    fi=$(echo $f | cut -d':' -f1);
+    l=$(echo $f | cut -d':' -f2- | sed 's/\./\\./g' | sed 's/\[/\\[/g'  | sed 's/\]/\\]/g');
+    sed -i "/^${l}$/d" $fi;
+done
+
+# removes tests with no test method name
+for f in $(grep  \\.$ *.csv ); do
+    fi=$(echo $f | cut -d':' -f1);
+    l=$(echo $f | cut -d':' -f2- | sed 's/\./\\./g');
+    sed -i "/^${l}$/d" $fi;
+done 
