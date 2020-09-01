6f57ef98c9a63bdc2e218e84985ba6e533c92420
winglam <winglam2@illinois.com>
2020-09-01 16:49:42 -0500

    Improving parse surefire script to handle when constructors fail.

--- python-scripts/parse_surefire_report.py
+++ python-scripts/parse_surefire_report.py
@@ -6,6 +6,8 @@ def output_xml_results(xml_file):
     with open(xml_file[1]) as fp:
         y=BeautifulSoup(fp, features="xml")
         # print str(y)
+        tout = []
+        failedconstructor = ""
         for f in y.testsuite.findAll("testcase"):
             s = "unknown"
             if f.find('failure'):
@@ -14,11 +16,19 @@ def output_xml_results(xml_file):
                 s = "error"
             else:
                 s = "pass"
-            if len(xml_file) == 4 and len(xml_file[3]) != 0:
+            if len(xml_file) == 4 and len(xml_file[3]) != 0 and (f["name"] == f["classname"] or f["name"] == ""):
                 t = xml_file[3]
+                failedconstructor = str.format("{},{},{},{},{}", t, s, f["time"], xml_file[2], xml_file[1])
+                break
             else:
                 t = str.format("{}.{}", f["classname"], f["name"])
-            print str.format("{},{},{},{},{}", t, s, f["time"], xml_file[2], xml_file[1])
+            tout.append(str.format("{},{},{},{},{}", t, s, f["time"], xml_file[2], xml_file[1]))
 
+        if failedconstructor != "":
+            print failedconstructor
+        else:
+            for f in tout:
+                print f
+            
 if __name__ == '__main__':
     output_xml_results(sys.argv)
