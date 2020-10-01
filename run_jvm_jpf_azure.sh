800b6a1ce29eb1947228eb74df7aa246f5ffcf1f
winglam <winglam2@illinois.com>
2020-09-30 22:25:16 -0500

    Adding azure script to run jvm/jpf

---
+++ run_jvm_jpf_azure.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+if [[ $1 == "" ]]; then
+    echo "arg1 - Path to CSV file with project,sha,test"
+    exit
+fi
+
+repo=$(git rev-parse HEAD)
+echo "script vers: $repo"
+dir=$(pwd)
+echo "script dir: $dir"
+starttime=$(date)
+echo "starttime: $starttime"
+
+RESULTSDIR=~/output/
+mkdir -p ${RESULTSDIR}
+
+cd ~/
+projfile=$1
+rounds=$2
+line=$(head -n 1 $projfile)
+
+echo "================Starting experiment for input: $line"
+slug=$(echo ${line} | cut -d',' -f1 | rev | cut -d'/' -f1-2 | rev)
+sha=$(echo ${line} | cut -d',' -f2)
+module=$(echo ${line} | cut -d',' -f3)
+
+MVNOPTIONS="-Ddependency-check.skip=true -Dgpg.skip=true -DfailIfNoTests=false -Dskip.installnodenpm -Dskip.npm -Dskip.yarn -Dlicense.skip -Dcheckstyle.skip -Drat.skip -Denforcer.skip -Danimal.sniffer.skip -Dmaven.javadoc.skip -Dfindbugs.skip -Dwarbucks.skip -Dmodernizer.skip -Dimpsort.skip -Dmdep.analyze.skip -Dpgpverify.skip -Dxml.skip -Dcobertura.skip=true -Dfindbugs.skip=true"
+
+modifiedslug=$(echo ${slug} | sed 's;/;.;' | tr '[:upper:]' '[:lower:]')
+short_sha=${sha:0:7}
+modifiedslug_with_sha="${modifiedslug}-${short_sha}"
+
+# echo "================Cloning the project"
+bash $dir/clone-project.sh "$slug" "$sha"
+cd ~/$slug
+
+if [[ -z $module ]]; then
+    echo "================ Missing module. Exiting now!"
+    exit 1
+else
+    echo "Module passed in from csv."
+fi
+echo "Location of module: $module"
+
+# echo "================Installing the project"
+bash $dir/install-project.sh "$slug" "$MVNOPTIONS" "$USER" "$module" "$sha" "$dir" "$fullTestName" "${RESULTSDIR}"
+ret=${PIPESTATUS[0]}
+mv mvn-install.log ${RESULTSDIR}
+if [[ $ret != 0 ]]; then
+    # mvn install does not compile - return 0
+    echo "Compilation failed. Actual: $ret"
+    exit 1
+fi
+
+# echo "================Running maven test"
+if [[ "$slug" == "dropwizard/dropwizard" ]]; then
+    # dropwizard module complains about missing dependency if one uses -pl for some modules. e.g., ./dropwizard-logging
+    MVNOPTIONS="${MVNOPTIONS} -am"
+elif [[ "$slug" == "fhoeben/hsac-fitnesse-fixtures" ]]; then
+    MVNOPTIONS="${MVNOPTIONS} -DskipITs"
+fi
+
+bash $dir/mvn-test.sh "$slug" "$module" "$testarg" "$MVNOPTIONS" "$ordering" "$sha" "$dir" "$fullTestName"
+ret=${PIPESTATUS[0]}
+cp mvn-test.log ${RESULTSDIR}
+testxml=$(find . -name TEST-*.xml | grep -E "target/surefire-reports/TEST-.*\.$class\.xml")
+if [[ -z $testxml ]]; then
+    # did not find
+    # mvn install compiles but test is not run from mvn test - return 0
+    echo "Passed compilation but cannot find an xml for the test class: $class"
+    exit 1
+fi
+
+# echo "================Parsing test list"
+bash $dir/parse-test-list.sh "$dir" "$fullTestName" "$RESULTSDIR"
+
+# echo "================Clone and run JVM/JPF"
+git clone https://github.com/y553546436/JPF_Homework.git
+bash JPF_Homework/run_projects/runproject.sh "$dir/JPF_Homework" "~/$slug" "$module" "${RESULTSDIR}"
+
+endtime=$(date)
+echo "endtime: $endtime"
