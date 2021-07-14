ff96fc5412dd8da931c3f67de428634755d26d51
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-14 21:24:46 +0300

    redundant slashes are removed

--- clone-project.sh
+++ clone-project.sh
@@ -7,19 +7,19 @@ cd ~/$AZ_BATCH_TASK_WORKING_DIR
 
 if [[ -d $slug ]]; then
     echo "The project is already in the working directory"
-    cd /$slug
+    cd $slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
-elif [[ ! -f "/$input_container/$modified_slug_sha_module.zip" ]]; then
+elif [[ ! -f "$input_container/$modified_slug_sha_module.zip" ]]; then
     git clone https://github.com/$slug $slug
-    cd /$slug
+    cd $slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
 else
     echo "$slug already exists in the container"
-    cp /$input_container/$modified_slug_sha_module.zip .
+    cp $input_container/$modified_slug_sha_module.zip .
     unzip -q $modified_slug_sha_module.zip
-    cd /$slug
+    cd $slug
     echo "SHA is $(git rev-parse HEAD)"
 fi
 if [[ "$(git rev-parse HEAD)" == "$sha" ]]; then
