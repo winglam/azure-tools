bd1edf81c88d9b7cd28147050f6fbc3773a38719
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-15 00:47:38 +0300

    redundant echo is removed

--- compile-project.sh
+++ compile-project.sh
@@ -67,7 +67,7 @@ ret=${PIPESTATUS[0]}
 cd ~/
 
 if [[ $ret != 0 ]]; then 
-    echo echo "$line,${modifiedslug_with_sha}=${modified_module},failed" | tee -a $AZ_BATCH_TASK_WORKING_DIR/$input_container/"$pool_id-results".txt
+    echo "$line,${modifiedslug_with_sha}=${modified_module},failed" | tee -a $AZ_BATCH_TASK_WORKING_DIR/$input_container/"$pool_id-results".txt
 else
     echo "$line,${modifiedslug_with_sha}=${modified_module},passed" | tee -a /$AZ_BATCH_TASK_WORKING_DIR/$input_container/"$pool_id-results".txt
 fi
