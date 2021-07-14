272d4e5240f3d14639e1a2d476d3440174f3601d
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-14 21:20:46 +0300

    clone-project.sh refactored

--- clone-project.sh
+++ clone-project.sh
@@ -3,24 +3,23 @@ modified_slug_sha_module=$2
 input_container=$3
 sha=$(echo $modified_slug_sha_module | rev | cut -d'=' -f2 | cut -d'-' -f1 | rev)
 
-cd ~/
+cd ~/$AZ_BATCH_TASK_WORKING_DIR
 
-if [[ -f "$AZ_BATCH_TASK_WORKING_DIR/$slug" ]]; then
+if [[ -d $slug ]]; then
     echo "The project is already in the working directory"
-    cd $AZ_BATCH_TASK_WORKING_DIR/$slug
+    cd /$slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
-elif [[ ! -f "$AZ_BATCH_TASK_WORKING_DIR/$input_container/$modified_slug_sha_module.zip" ]]; then
+elif [[ ! -f "/$input_container/$modified_slug_sha_module.zip" ]]; then
     git clone https://github.com/$slug $slug
-    cd $AZ_BATCH_TASK_WORKING_DIR/$slug
+    cd /$slug
     git checkout $sha
     echo "SHA is $(git rev-parse HEAD)"
 else
     echo "$slug already exists in the container"
-    cp $AZ_BATCH_TASK_WORKING_DIR/$input_container/$modified_slug_sha_module.zip .
-    unzip $modified_slug_sha_module.zip
-    cd $AZ_BATCH_TASK_WORKING_DIR/$slug
-    git checkout $sha
+    cp /$input_container/$modified_slug_sha_module.zip .
+    unzip -q $modified_slug_sha_module.zip
+    cd /$slug
     echo "SHA is $(git rev-parse HEAD)"
 fi
 if [[ "$(git rev-parse HEAD)" == "$sha" ]]; then
