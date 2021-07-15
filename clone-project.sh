e0a2d4b396d7e7c7153a2725fa7661d476af3395
huseyingokay <huseyin.gokay@ozu.edu.tr>
2021-07-15 09:55:41 +0300

    bugfix in clone-project.sh

--- clone-project.sh
+++ clone-project.sh
@@ -3,7 +3,7 @@ modified_slug_sha_module=$2
 input_container=$3
 sha=$(echo $modified_slug_sha_module | rev | cut -d'=' -f2 | cut -d'-' -f1 | rev)
 
-cd ~/$AZ_BATCH_TASK_WORKING_DIR
+cd ~/
 
 if [[ -d $slug ]]; then
     echo "The project is already in the working directory"
